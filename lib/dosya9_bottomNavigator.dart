ac05d180412a26b4c6af94900ec7f0fcc779d686
besyildiz <mehterli@gmail.com>
2022-11-10 13:54:01 +0300

    Update dosya9_bottomNavigator.dart

--- lib/dosya9_bottomNavigator.dart
+++ lib/dosya9_bottomNavigator.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(calismam1());
+}
+
+TextEditingController sayi3 = TextEditingController();
+TextEditingController sayi2 = TextEditingController();
+TextEditingController sayi1 = TextEditingController();
+
 class calismam1 extends StatefulWidget {
   const calismam1({super.key});
 
@@ -11,26 +19,44 @@ class _calismam1State extends State<calismam1> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Demo'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
+          appBar: AppBar(
+            title: const Text('BottomNavigationBar Demo'),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'Camera',
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.call),
+                label: 'Calls',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: 'Camera',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat),
+                label: 'Chats',
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+          body: Column(children: [
+            TextField(
+              controller: sayi1,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: "1. sayıyı giriniz"),
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Sayı mı değil mi?",
+                  style: TextStyle(backgroundColor: Colors.amber),
+                )),
+          ]),
+        ));
   }
 }
